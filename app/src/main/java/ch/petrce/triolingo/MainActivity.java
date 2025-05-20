ff833f73cc1d84778b8d3efeff2ca4a386fe6fc2
Petr Cerny <petr.cerny@e3ag.ch>
2025-05-20 18:04:18 +0200

    added sensor to app

--- app/src/main/java/ch/petrce/triolingo/MainActivity.java
+++ app/src/main/java/ch/petrce/triolingo/MainActivity.java
@@ -1,10 +1,14 @@
 package ch.petrce.triolingo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,12 +19,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private Integer currentIndex = 0;
     private List<Vocab> vocabList;
     private TextView wordText;
     private TextView translationText;
-
+    private View rootView;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +38,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        rootView = findViewById(R.id.main); // get layout from xml
         wordText = findViewById(R.id.wordText); // get wordText from xml
         translationText = findViewById(R.id.translationText); // get translationText from xml
         vocabList = JsonLoader.loadVocabulary(this); // call loadfunction for vocablist
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // initialize sensorManager
+        if(sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); // get accelerometer sensor
+        }
 
         showCurrentVocab(); // call function to show first vocab
     }
@@ -48,4 +60,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this); // unregister sensor listener
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0]; // Links/Rechts Kippen
+
+            if (x > 3) {
+                rootView.setBackgroundColor(Color.parseColor("#553b3c")); // set background color to red
+                Log.d("Triolingo", "Kippe nach links erkannt");
+            } else if (x < -3) {
+                rootView.setBackgroundColor(Color.parseColor("#3d553d")); // set background color to green
+                Log.d("Triolingo", "Kippe nach rechts erkannt");
+            } else {
+                rootView.setBackgroundColor(Color.parseColor("#3c3e55")); // keep background color
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 }
